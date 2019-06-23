4427968d7fef9f83dc8dbf154cd55766ff1861b3
yunyoung1819 <yunyoung1819@hanmail.net>
2019-06-24 01:35:33 +0900

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                일정 관리
+            </div>
+        )
+    }
+}
+
+export default App;
