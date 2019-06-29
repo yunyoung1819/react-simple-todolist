2e7847f54da67fd81bcf84a722a5354b520d1f41
yunyoung1819 <yunyoung1819@hanmail.net>
2019-06-29 11:29:14 +0900

    PageTemplate.scss add

--- src/components/PageTemplate/PageTemplate.js
+++ src/components/PageTemplate/PageTemplate.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './PagaTemplate.scss';
+import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
